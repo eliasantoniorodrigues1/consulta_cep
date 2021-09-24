d65ab0ba5f927ce5b5853bf91fba764134a9723e
Elias Rodrigues <49626719+eliasantoniorodrigues1@users.noreply.github.com>
2021-09-24 15:17:28 -0300

    Update consulta_cep.py

--- consulta_cep.py
+++ consulta_cep.py
@@ -6,7 +6,7 @@ import re
 cep = "32187120"
 url = f"https://www.cepaberto.com/api/v3/cep?cep={cep}"
 # O seu token está visível apenas pra você
-headers = {'Authorization': 'Token token=b307af069f8f5e60c4c03be00251e29b'}
+headers = {'Authorization': 'Token token=...'}
 response = requests.get(url, headers=headers)
 
 print(response.json())
